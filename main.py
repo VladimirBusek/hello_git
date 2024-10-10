1b4b9a56d2e07e34c6ec0ad93b270cef8dc52561
Václav Tomaník <tomanva.2025@guh.cz>
2024-10-10 15:11:23 +0200

    zprava od vency

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.venca as venca
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+venca.muj_pozdrav()
