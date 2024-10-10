f212cb7bfe27cf485331f3755b4faf6ee6e47324
Vrága Tomáš <vragato.2025@guh.cz>
2024-10-10 15:11:51 +0200

    zpráva z tom

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.tomas as tomas
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+tomas.muj_pozdrav()
