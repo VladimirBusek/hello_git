48702fff20b301a1ab90e49b25cb656df2d9d815
Grigorov Boris <grigobo.2025@guh.cz>
2024-10-10 15:11:51 +0200

    Od Buška

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.budek as lide
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+busek.muj_pozdrav()
