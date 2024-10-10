3c754b187b80d885237894c41f4201d6aba9354c
Krchňáček Jakub <krchnja.2025@guh.cz>
2024-10-10 15:13:55 +0200

    Zprava

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import lide.boris as boris
+import lide.krchnacek as krchnacek
+
+
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+krchnacek.muj_pozdrav()
