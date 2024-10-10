047a0145b3b6a609b8f7af2a398817a2191c1f04
Babula Vlastimil <babulvl.2025@guh.cz>
2024-10-10 15:11:29 +0200

    Dalsi zprava

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.vlastimil as vlastimil
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+vlastimil.muj_pozdrav()
