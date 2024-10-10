f902dee9098b9eadb73e68f899ec97cdaedba684
Černušková Daniela <cernuda.2025@guh.cz>
2024-10-10 15:11:29 +0200

    pozdrav od Danči

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.daniela as daniela
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+daniela.muj_pozdrav()
