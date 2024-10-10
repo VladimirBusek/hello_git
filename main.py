e2fb63db6d05d1db8a78ed3633cf5ebd6e7efec9
Simon Šmíd <smid.simon@centrum.cz>
2024-10-10 15:11:41 +0200

    caues brasko

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import lide.boris as boris
+import lide.simon as simon
 
 print("Ahoj, já jsem main.py, tady jsou zprávy od spolupracovníků na tomto projektu:")
 print("-------------------------")
 print()
 
 boris.muj_pozdrav()
+simon.muj_pozdrav()
